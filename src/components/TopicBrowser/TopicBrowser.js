4d78299e877421c9d7b54bc36a770b8910146ebd
Shannon Scott <sean3rd0@icloud.com>
2019-09-06 16:59:09 -0600

    fixed a bug :)

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import EvenAndOdd from '../Topics/EvenAndOdd'
 import FilterObject from '../Topics/FilterObject'
-import FilterString from '../Topics/FilterObject'
+import FilterString from '../Topics/FilterString'
 import Palindrome from '../Topics/Palindrome'
 import Sum from '../Topics/Sum'
 export default class TopicBrowser extends Component {
